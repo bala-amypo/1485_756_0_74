ae68368b6556a429e7f0d0537300898fc5113894
bala-amypo <bala@amypo.in>
2025-12-17 08:15:34 +0000

    User commit

--- src/main/java/com/example/demo/service/LocationServImp.java
+++ src/main/java/com/example/demo/service/LocationServImp.java
@@ -1,4 +1,4 @@
-package com.example.demo.sevice;
+package com.example.demo.service;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.entity.Location;
